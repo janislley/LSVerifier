194bdbe30ad716feb5ee50de198cc1d3828635ac
Thales Silva <thales.tas@gmail.com>
2021-02-24 22:37:16 -0400

    Added property in dict

--- frama-wr.py
+++ frama-wr.py
@@ -7,6 +7,7 @@ import re
 FILE = "file"
 LINE = "line"
 FUNCTION = "function"
+PROPERTY = "property"
 
 def search_in_file(fname, string):
     pattern = re.compile(string)
@@ -33,16 +34,17 @@ def read_lines(fname, lines):
 def counter_to_dict(line, prop):
     """docstring for dict_counter"""
     list_dict = []
-    for i in line:
+    for i, j in zip(line, prop):
         if not (len(i) == 0):
             counters = {}
             counters[FILE] = i.split()[1]
             counters[LINE] = i.split()[3]
             counters[FUNCTION] = i.split()[5]
+            counters[PROPERTY] = j.strip()
 
             list_dict.append(counters)
 
-    print(list_dict)
+    return list_dict
 
 def main():
     print("Running...")
@@ -50,13 +52,14 @@ def main():
     fname = "output/output.log"
     string = "Violated property"
 
-    cex_line = search_in_file(fname, string)
-    print(cex_line)
-
+    cex_location_line = search_in_file(fname, string)
+    cex_location = read_lines(fname, cex_location_line)
+    
+    cex_line = [n+1 for n in cex_location_line]
     cex = read_lines(fname, cex_line)
-    print(cex)
 
-    counter_to_dict(cex, 1)
+    cex_dict = counter_to_dict(cex_location, cex)
+    print(cex_dict)
 
 
 if __name__ == "__main__":
