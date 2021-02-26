d5dc5dcdfe1c5d426e4efb7cc7c7bf2728ca5ebc
Erickson Alves <erickson.higor@gmail.com>
2021-02-25 20:24:36 -0400

    Handle multiple invariant values
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- instrumenter.py
+++ instrumenter.py
@@ -17,20 +17,21 @@ def list2code(lines, filename):
         file.write(line)
     file.close()
 
-# instrument code with assumes w.r.t. given map
-def instrument_code(filename, map):
+# instrument code with assumes w.r.t. given invariants
+def instrument_code(filename, invariants):
     print('Instrumenting source-code with given parameters')
-    line_numbers = set([line_number for line_number in map])
+    line_numbers = set([line_number for line_number in invariants])
     lines = code2list(filename)
     instrumented = []
     for idx in range(0, len(lines)):
         if idx + 1 in line_numbers:
-            instrumentation = map[idx + 1]
+            instrumentation = invariants[idx + 1]
             var_name = instrumentation[0]
-            var_value = instrumentation[1]
+            var_values = instrumentation[1]
             spaces = len(lines[idx]) - len(lines[idx].lstrip())
-            assume = '__ESBMC_assume({0} == {1});\n'.format(var_name, var_value)
-            instrumented.append(' ' * spaces + assume)
+            for var_value in var_values:
+                assume = '__ESBMC_assume({0} == {1});\n'.format(var_name, var_value)
+                instrumented.append(' ' * spaces + assume)
         instrumented.append(lines[idx])
     basename = os.path.basename(filename)
     dirname = os.path.dirname(filename)
