facff5c89273ee2de102befac0fc3674b5350647
Thales Silva <thales.tas@gmail.com>
2021-06-24 22:56:51 -0400

    move some functions to utils.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,76 +3,20 @@ import glob
 import time
 import argparse
 import shlex
-import sys
 import os
-from shutil import which
 from src.csvwr import csvwr
 from src.log import log
 from src.bar import Bar
-from pathlib import Path
-
-CTAGS = "ctags"
-CTAGS_TAB = "-x"
-CTAGS_FUNC = "--c-types=f"
+from src.utils import utils
 
 ESBMC = "esbmc"
+
 FUNCTION = "--function"
 NO_POINTER = "--no-pointer-check"
 
 POINTER_FAIL = "invalid pointer"
 
 DEP = "-I"
-
-def get_command_line(args):
-    cmd_line = ""
-
-    if args.esbmc_parameter:
-        para = args.esbmc_parameter
-        para = para.split(" ")
-        for i in range(len(para)):
-            cmd_line += para[i] + " "
-
-    return(cmd_line)
-
-def read_dep_file(path):
-    dep_file = open(path, "r")
-    dep_list = [x.strip() for x in dep_file.readlines()]
-
-    for i in range(len(dep_list)):
-        dep_list.insert(2 * i, "-I")
-
-    return(dep_list)
-
-def list_functions(c_file):
-    process = subprocess.Popen([CTAGS,CTAGS_TAB,CTAGS_FUNC,c_file],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True)
-
-    (stdout, stderr) = process.communicate()
-
-    func_list = row_2_list(stdout)
-    func_list = find_main(func_list)
-
-    return(func_list)
-
-def row_2_list(text):
-    func = list()
-
-    for row in text.split("\n"):
-        if " " in row:
-            key, value = row.split(" ",1)
-            func.append(key)
-
-    return(func)
-
-def find_main(f_list):
-    item = "main"
-
-    if item in f_list:
-        f_list.insert(0, f_list.pop(f_list.index(item)))
-
-    return(f_list)
 
 def run(cmd):
     invalid_pointer = 0
@@ -96,7 +40,7 @@ def run_esbmc(c_file, cmd_line, dep_list, args):
     if not args.functions:
         func_list = ["main"]
     else:
-        func_list = list_functions(c_file)
+        func_list = utils.list_functions(c_file)
 
     esbmc_args = shlex.split(cmd_line);
 
@@ -120,23 +64,6 @@ def run_esbmc(c_file, cmd_line, dep_list, args):
 
         log.info("")
 
-def list_c_files(recursive, directory):
-    file_list = []
-
-    if recursive:
-        for path in Path(".").rglob("*.c"):
-           file_list.append(str(path))
-    elif directory:
-        file_list = glob.glob(directory + "/*.c")
-    else:
-        file_list = glob.glob("*.c")
-
-    if not len(file_list):
-        log.error("There is not .c file here!!")
-        sys.exit()
-
-    return(file_list)
-
 def arguments():
     parser = argparse.ArgumentParser("Input Options")
     parser.add_argument("-e", "--esbmc-parameter", help="Use ESBMC parameter")
@@ -151,13 +78,8 @@ def arguments():
 
     return(args)
 
-def is_esbmc_installed():
-    if not which(ESBMC) is not None:
-        print("ESBMC not found!!")
-        sys.exit()
-
 def main():
-    is_esbmc_installed()
+    utils.is_esbmc_installed()
 
     args = arguments()
 
@@ -166,12 +88,12 @@ def main():
     print("ESBMC Running...")
 
     # Format ESBMC arguments
-    cmd_line = get_command_line(args)
+    cmd_line = utils.get_command_line(args)
 
     # Read Libraries Dependencies File
     if args.libraries:
         log.info("Dependecies File: %s" % args.libraries)
-        dep_list = read_dep_file(args.libraries)
+        dep_list = utils.read_dep_file(args.libraries)
     else:
         dep_list = []
 
@@ -179,7 +101,7 @@ def main():
     if args.file:
         all_c_files = [args.file]
     else:
-        all_c_files = list_c_files(args.recursive, args.directory)
+        all_c_files = utils.list_c_files(args.recursive, args.directory)
 
 
     start_all = time.time()
